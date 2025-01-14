bb1d8d2132e2ded5ee52ac81b106b7276d38a3bb
Alberto <albertojunior848423803@gmail.com>
2025-01-14 19:27:31 +0300

    changes

--- src/main/java/com/example/loginauthapi/controllers/AuthController.java
+++ src/main/java/com/example/loginauthapi/controllers/AuthController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
+
 @Data
 @RestController
 @RequestMapping("/auth")
